769f37a55572a0746104cc362efeba9fd6a44737
HRITIK PANDEY <2024.hritik.pandey@ves.ac.in>
2025-11-16 04:15:46 +0530

    Update 18,24_selectionSort.c

--- 18,24_selectionSort.c
+++ 18,24_selectionSort.c
@@ -2,24 +2,18 @@
 
 #include <stdio.h>
 
-void swap(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
 void selectionSort(int arr[], int n) {
-    int i, j, min_idx;
-
-    for (i = 0; i < n - 1; i++) {
-        min_idx = i;
-        for (j = i + 1; j < n; j++) {
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) {
             if (arr[j] < arr[min_idx])
                 min_idx = j;
         }
-
-        if (min_idx != i)
-            swap(&arr[min_idx], &arr[i]);
+        if (min_idx != i){
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
     }
 }
 
